bcfad36959ac0b2234d6cd4667c647b2e773fb12
Tarik Palic <tarik.palic@intertops.ag>
2020-01-24 14:39:05 +0100

    changes done at work

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-  console.log('[Persons.js] rendering...');   
-  return props.persons.map( (person,index) => {   
-  return <Person 
-        key = {person.id}
-        click = {() => props.clicked(index)}
-        name={person.name} 
-        age = {person.age}
-        changed = {(event) => props.changed(event,person.id)} />
-      });
-    }
+class Persons extends Component {
 
-      export default persons;
+  static getDerivedStateFromProps(props, state){
+    return state;
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    if (nextProps.persons !== this.props.persons){
+      return true;
+    }
+    else{
+      return false;
+    }   
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...');
+    return this.props.persons.map((person, index) => {
+      return <Person
+        key={person.id}
+        click={() => this.props.clicked(index)}
+        name={person.name}
+        age={person.age}
+        changed={(event) => this.props.changed(event, person.id)} />
+    });
+  }
+
+}
+
+export default Persons;
